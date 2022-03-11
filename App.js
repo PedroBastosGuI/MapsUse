e7a404cf4ef89bcbd458ec9aa446135b43f4e3f1
Pedro Guilherme <pedrobastos750@gmail.com>
2022-03-11 16:43:43 -0400

    routes

--- App.js
+++ App.js
@@ -3,25 +3,25 @@ import { Alert, StyleSheet, Text, View } from 'react-native';
 import {Platform, PermissionsAndroid, Dimensions} from 'react-native';
 import MapView from 'react-native-maps';
 import * as Location from "expo-location";
-
+import MapViewDirections from 'react-native-maps-directions';
 
 const{height,width} = Dimensions.get('screen');
 
+const GOOGLE_API_KEY = 'AIzaSyA1Y62ie7t-jwcm7_KRkojSn4fiUgQoqtE';
+
+const CedirLabsOrigin = {latitude:-16.461715, longitude:-54.638717};
+
+const origin = {latitude: 37.3318456, longitude: -122.0296002};
+const destination = {latitude: 37.771707, longitude: -122.4053769};
 
 
-const initialRegion = {
-  latitude: -15.5989,
-  longitude: -56.0949,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
-};
 
 export default function App() {
 
   // estado para armazenar a position 
   const [location, setLocation] = useState(null);
   const [region, setRegion] = useState();
-
+  const[coordenadas, setCoordenadas] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -34,30 +34,23 @@ export default function App() {
       let{
         coords:{latitude,longitude}
       } = await Location.getCurrentPositionAsync();
-      setLocation({latitude,longitude, latitudeDelta:100, longitudeDelta:100});
-      
+      setLocation({latitude,longitude, latitudeDelta:0.0922, longitudeDelta:0.0421});
+      setCoordenadas({latitude,longitude})
       setRegion({
         latitude,
         longitude,
-        latitudeDelta: 3,
-        longitudeDelta: 3,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
       })
     })();
   }, []);
 
   let text = 'Waiting..';
-
   if (location) {
     text = JSON.stringify(location);
-    console.log(text);
   }
 
-
-
-
-
-
- 
+  //const origin = coordenadas;
   return (
     <View style={styles.container}>
       <MapView
@@ -75,7 +68,13 @@ export default function App() {
         minZoomLevel={17}
         showsUserLocation={true}
         loadingEnabled={true}
-      />
+      >
+        <MapViewDirections
+            origin={origin}
+            destination={destination}
+            apikey={GOOGLE_API_KEY}
+        />
+      </MapView>
     </View>
   );
 }
